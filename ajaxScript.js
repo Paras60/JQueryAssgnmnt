d15c6c7f4597a5eedffac2b94b2d32830d16f2ea
Praveen <praveen.jujare11@gmail.com>
2016-07-20 18:06:16 +0530

    first commit

--- ajaxScript.js
+++ ajaxScript.js
@@ -20,17 +20,13 @@ $(document).ready(function(){
 		            		var table = document.getElementById("myTable");
 		            		var row = table.insertRow(l);
 
-							var cell5 = row.insertCell(0);
+							var cell1 = row.insertCell(0);
 							var cell2 = row.insertCell(1);
-							var cell3 = row.insertCell(2);
-							var cell4 = row.insertCell(3);
-							var cell1 = row.insertCell(4);
+							
 
-							cell1.innerHTML = json[l].Title;
-							cell2.innerHTML = json[l].Year;
-							cell3.innerHTML = json[l].imdbID;
-							cell4.innerHTML = json[l].Type;
-							cell5.innerHTML = "<img src="+json[l].Poster+">";
+							cell2.innerHTML = "Name : "+json[l].Title+"<br> Year : "+json[l].Year+"<br> imdbID : "+json[l].imdbID+"<br> Type : "+json[l].Type;
+                        
+							cell1.innerHTML = "<img src="+json[l].Poster+">";
 
 							
 							l=l+1;
